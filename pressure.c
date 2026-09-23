d836c370447a42845108fb71a6afb273970cf7e5
Leah Neukirchen <leah@vuxu.org>
2026-09-23 15:05:29 +0200

    pressure: use sscanf and loops
    This is more robust against potential input changes, a failing fscanf
    can result in indeterminate file position.

--- pressure.c
+++ pressure.c
@@ -26,25 +26,33 @@ static void pressure_collect(scrape_req *req, void *ctx) {
   FILE *f;
   long long ms;
 
+  char line[BUF_SIZE];
+
   if ((f = fopen("/proc/pressure/cpu", "r"))) {
-    if (fscanf(f, "some %*[^t]total=%lld\n", &ms) == 1)
-      scrape_write(req, "node_pressure_cpu_waiting_seconds_total", 0, ms / 1e6);
+    while (fgets_line(line, sizeof line, f)) {
+      if (sscanf(line, "some %*[^t]total=%lld\n", &ms) == 1)
+        scrape_write(req, "node_pressure_cpu_waiting_seconds_total", 0, ms / 1e6);
+    }
     fclose(f);
   }
 
   if ((f = fopen("/proc/pressure/memory", "r"))) {
-    if (fscanf(f, "some %*[^t]total=%lld\n", &ms) == 1)
-      scrape_write(req, "node_pressure_memory_waiting_seconds_total", 0, ms / 1e6);
-    if (fscanf(f, "full %*[^t]total=%lld\n", &ms) == 1)
-      scrape_write(req, "node_pressure_memory_stalled_seconds_total", 0, ms / 1e6);
+    while (fgets_line(line, sizeof line, f)) {
+      if (sscanf(line, "some %*[^t]total=%lld\n", &ms) == 1)
+        scrape_write(req, "node_pressure_memory_waiting_seconds_total", 0, ms / 1e6);
+      else if (sscanf(line, "full %*[^t]total=%lld\n", &ms) == 1)
+        scrape_write(req, "node_pressure_memory_stalled_seconds_total", 0, ms / 1e6);
+    }
     fclose(f);
   }
 
   if ((f = fopen("/proc/pressure/io", "r"))) {
-    if (fscanf(f, "some %*[^t]total=%lld\n", &ms) == 1)
-      scrape_write(req, "node_pressure_io_waiting_seconds_total", 0, ms / 1e6);
-    if (fscanf(f, "full %*[^t]total=%lld\n", &ms) == 1)
-      scrape_write(req, "node_pressure_io_stalled_seconds_total", 0, ms / 1e6);
+    while (fgets_line(line, sizeof line, f)) {
+      if (sscanf(line, "some %*[^t]total=%lld\n", &ms) == 1)
+        scrape_write(req, "node_pressure_io_waiting_seconds_total", 0, ms / 1e6);
+      else if (sscanf(line, "full %*[^t]total=%lld\n", &ms) == 1)
+        scrape_write(req, "node_pressure_io_stalled_seconds_total", 0, ms / 1e6);
+    }
     fclose(f);
   }
 }
